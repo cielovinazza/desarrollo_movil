8b20725278f173fef1f3f06d9965b497698a9194
Cristobal Quezada <cristobal.qsilva@gmail.com>
2026-05-10 00:13:19 -0400

    mock backend implementado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
-import 'app/app.dart';
+
+import 'package:project/features/cliente/data/datasources/clientes_local_datasource.dart';
+import 'package:project/features/cliente/data/repositories/clientes_repository_impl.dart';
+import 'package:project/features/cliente/domain/usecases/get_clientes_usecase.dart';
+import 'package:project/features/cliente/presentation/pages/clientes_page.dart';
 
 void main() {
-  runApp(const MyApp());
+
+  final dataSource = ClientesLocalDataSource();
+  final repository = ClientesRepositoryImpl(dataSource);
+  final useCase = GetClientesUseCase(repository);
+
+  runApp(MyApp(useCase: useCase));
+}
+
+class MyApp extends StatelessWidget {
+
+  final GetClientesUseCase useCase;
+
+  const MyApp({super.key, required this.useCase});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ClientesPage(useCase: useCase),
+    );
+  }
 }
